d12c9a10761d2c7cc04eabec36b4b001c5055d4d
Artem <footerka2@gmail.com>
2021-10-10 22:46:56 +0300

    Create base repository

---
+++ src/main/java/com/shop/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.shop.repository;
+
+import com.shop.entity.Product;
+import com.shop.entity.ValueCharacteristic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+      List<Product> findByValueCharacteristicIn(Iterable<ValueCharacteristic> valueCharacteristics);
+}
