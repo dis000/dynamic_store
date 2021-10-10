36eacc35ee45e92bd74844ad5309b9f42cd40183
Artem <footerka2@gmail.com>
2021-10-10 21:12:14 +0300

    Create Product

--- src/main/java/com/shop/entity/Product.java
+++ src/main/java/com/shop/entity/Product.java
@@ -1,5 +1,36 @@
 package com.shop.entity;
 
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column("id")
+    private Long id;
+
+    @Column("name")
+    @NonNull
+    private String name;
+
+    @Column("price")
+    @NotNull
+    private Integer price;
+
+    @Column("type")
+    @NotNull
+    private String type;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<ValueCharacteristic> valueCharacteristic;
 }
